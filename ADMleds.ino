8978675d60ec8a157cc801c1468242a383382645
Adam Sidor <sidoadsi1@gmail.com>
2024-12-25 19:57:50 +0100

    Wifi included

--- ADMleds.ino
+++ ADMleds.ino
@@ -1,6 +1,16 @@
-void setup() {
-  
+#include <WiFi.h>
+#include "webConfig.h"
 
+WiFiServer server(80);
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); 
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(10);
+  }
+  Serial.println();
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
